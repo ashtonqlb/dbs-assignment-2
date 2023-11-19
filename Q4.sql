35449c2ed080fdc708bd9613700fdb7b06d46c7e
Ashton Bennet <abennet@myseneca.ca>
2023-11-19 18:39:24 -0500

    Cosmetic changes to Q4 and Q6 faneto

--- Q4.sql
+++ Q4.sql
@@ -1,26 +1,23 @@
 -- DBS311NEE Assignment 2 - Task 4
--- Author: Liam Toye (lctoye@myseneca.ca)
+-- Author: Liam Toye (lctoye@myseneca.ca) & Ashton Lunken (abennet@myseneca.ca)
 
 -- 4. Create a view which stores the “players on teams” information, called vwPlayerRosters
 -- which includes all fields from players, rosters, and teams in a single output table.
 -- You only need to include records that have exact matches.
 
-CREATE OR REPLACE VIEW vwplayerrosters AS
+CREATE OR REPLACE VIEW vwPlayerRosters AS
     SELECT
-        players.playerid     playerid,
-        players.regnumber    regnumber,
-        players.lastname     lastname,
-        players.firstname    firstname,
-        players.isactive     isactive,
-        rosters.rosterid     rosterid,
-        rosters.teamid       teamid,
-        rosters.jerseynumber jerseynumber,
-        teams.teamname       teamname,
-        teams.jerseycolour   jerseycolour
+        p.playerid     playerid,
+        p.regnumber    regnumber,
+        p.lastname     lastname,
+        p.firstname    firstname,
+        p.isactive     isactive,
+        r.rosterid     rosterid,
+        r.teamid       teamid,
+        r.jerseynumber jerseynumber,
+        t.teamname     teamname,
+        t.jerseycolour jerseycolour
     FROM
-        players,
-        rosters,
-        teams
-    WHERE
-        players.playerid = rosters.playerid
-        AND rosters.teamid = teams.teamid;
+        players p
+    INNER JOIN rosters r ON p.playerid = r.playerid
+    INNER JOIN teams t ON r.teamid = t.teamid;
--- Q6.sql
+++ Q6.sql
@@ -1,6 +1,52 @@
 -- DBS311NEE Assignment 2 - Task 6
 -- Author: Ashton Lunken (abennet@myseneca.ca)
 
-/* 6. Repeat task 4, by creating another similar stored procedure, named spTeamRosterByName, that receives 
-a string parameter and returns the team roster, with names, for a team found through a search string. 
-The entered parameter may be any part of the name. */
+/**
+ * Executes the 'spTeamRosterByName' stored procedure to fetch and display the team roster.
+ * This procedure searches the 'vwPlayerRosters' view for a team that includes the specified
+ * string in its name and prints out the roster to DBMS_OUTPUT.
+ *
+ * @param p_team_name A partial or full team name to search for within the team names.
+ * 
+ * @note It is case-insensitive.
+ * 
+ */
+
+CREATE OR REPLACE PROCEDURE spTeamRosterByName (
+    p_team_name IN VARCHAR2
+) AS
+    v_result_quantity INTEGER := 0;
+    CURSOR cTeamRoster IS
+        SELECT
+            playerid,
+            regnumber,
+            lastname,
+            firstname,
+            isactive,
+            rosterid,
+            teamid,
+            jerseynumber,
+            teamname,
+            jerseycolour
+        FROM
+            vwPlayerRosters
+        WHERE
+            LOWER(teamname) LIKE LOWER('%' || p_team_name || '%');
+BEGIN
+    FOR roster_record IN cTeamRoster LOOP
+        v_result_quantity := v_result_quantity + 1; -- Increment the counter
+        DBMS_OUTPUT.PUT_LINE
+        (
+            'Result ' || TO_CHAR(v_result_quantity) || CHR(10) 
+            || 'Name: ' || roster_record.firstname || ' ' || roster_record.lastname || CHR(10)
+            || 'Jersey Number: ' || roster_record.jerseynumber || CHR(10)
+            || 'Team: ' || roster_record.teamname || CHR(10)
+        );
+    END LOOP;
+END;
+/
+
+BEGIN 
+    spTeamRosterByName('Aurora');
+END;
+/
