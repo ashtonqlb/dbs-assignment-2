577c8d25a4c3354a2dc53f11ba627f54fc423b3c
Ashton Bennet <abennet@myseneca.ca>
2023-11-20 13:24:53 -0500

    Q9 and Q10 done

--- Final Submission.sql
+++ Final Submission.sql
@@ -348,11 +348,80 @@ WHEN OTHERS THEN
 END;
 /
 
+-- Q9 
+
+CREATE OR REPLACE VIEW vwSchedule AS
+    SELECT
+        g.gamenum,
+        g.gamedatetime,
+        g.hometeam,
+        ht.teamname as HomeTeamName,
+        g.visitteam,
+        vt.teamname as VisitTeamName,
+        l.locationname
+    FROM 
+        GAMES g
+    INNER JOIN SLLOCATIONS l ON g.locationid = l.locationid
+    INNER JOIN TEAMS ht ON g.hometeam = ht.teamid
+    INNER JOIN TEAMS vt ON g.visitteam = vt.teamid
+    ORDER BY g.gamedatetime;
+
+/**
+ * Q10
+ * Retrieves and displays games scheduled to occur within the next 'n' days (using SYSDATE() for the current date), 
+ * by defining a cursor to select all fields from vwSchedule into the procedure variables.
+ *
+ * @param n The number of days ahead of the current date to retrieve games for.
+ *          Must be a positive integer representing the days to look ahead.
+ *          
+ * @note The actual column widths for 'v_home_team_name' and 'v_visit_team_name' must be
+ * long enough to store the longest team name in the database. Adjust the VARCHAR2 size
+ * if the team names are getting truncated.
+ */
+
+CREATE OR REPLACE PROCEDURE spSchedUpcomingGames(
+    n IN INTEGER
+) IS
+  CURSOR cUpcomingGames IS
+      SELECT gamenum, gamedatetime, HomeTeamName, VisitTeamName
+      FROM vwSchedule
+      WHERE gamedatetime BETWEEN SYSDATE AND SYSDATE + n
+      ORDER BY gamedatetime;
+  v_game_num INTEGER;
+  v_game_date_time DATE;
+  v_home_team_name VARCHAR2(10);
+  v_visit_team_name VARCHAR2(10);
+BEGIN
+  OPEN cUpcomingGames;
+
+  LOOP
+    FETCH cUpcomingGames INTO v_game_num, v_game_date_time, v_home_team_name, v_visit_team_name;
+    EXIT WHEN cUpcomingGames%NOTFOUND;
+
+    DBMS_OUTPUT.PUT_LINE(
+        'Game Number: ' || v_game_num || 
+        ', Date: ' || TO_CHAR(v_game_date_time, 'DD-MON-YYYY') || 
+        ', Home Team: ' || v_home_team_name || 
+        ', Visit Team: ' || v_visit_team_name
+    );
+  END LOOP;
+  CLOSE cUpcomingGames;
+EXCEPTION
+  WHEN OTHERS THEN
+    IF cUpcomingGames%ISOPEN THEN
+      CLOSE cUpcomingGames;
+    END IF;
+END spSchedUpcomingGames;
+/
+
 -- Sample execution code
 
 BEGIN
     spteamrosterbyid(210);
     spTeamRosterByName('Aurora');
+    SELECT * FROM vwTeamsNumPlayers
     SELECT fncNumPlayersByTeamID(216) FROM DUAL;
+    SELECT * FROM vwSchedule;
+    spSchedUpcomingGames(7);
 END;
 /
